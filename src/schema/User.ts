a52fc7456f58380a343eee6a51fa99b6f0487d78
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-08-12 18:48:41 +0600

    [db]: set `_id` of type string;

--- src/schema/User.ts
+++ src/schema/User.ts
@@ -2,7 +2,7 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
 @Schema({ timestamps: true })
-export class User extends Document {
+export class User extends Document<string> {
   @Prop({ unique: true, required: true })
   handle: string;
 
