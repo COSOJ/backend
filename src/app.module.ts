f5bfd812db8d76c84e5dad70f01bc3182f27fcc4
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-08-15 10:10:30 +0600

    [root]: registered auth, problem, submission controller;

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthController } from './controller/auth.controller';
+import { ProblemController } from './controller/problem.controller';
+import { SubmissionController } from './controller/submission.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [
+    AppController,
+    AuthController,
+    ProblemController,
+    SubmissionController,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
