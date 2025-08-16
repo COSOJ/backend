38980701f78d3024d09b3600eef3f38518e1c93e
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-08-16 19:26:20 +0600

    [cors]: fixed cors issues;

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// todo: use config file to handle our sites only
+const allowedOrigins = [
+  'http://localhost:5173',
+];
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // todo: use config file to handle our sites only
-  app.enableCors();
+  app.enableCors({
+    origin: allowedOrigins,
+    credentials: true, // for cookies and headers
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
