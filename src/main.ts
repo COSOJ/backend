2e5269e43541a4c0be9e2d46d12b96554e8cf277
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-08-16 19:14:09 +0600

    [cors]: enabled cors;

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // todo: use config file to handle our sites only
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
