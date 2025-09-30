b57503a8253febdefe392f968c9f8cf774ea9a05
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-09-30 21:22:01 +0600

    [feature]: added public, private test cases;

--- src/schema/Problem.ts
+++ src/schema/Problem.ts
@@ -1,6 +1,12 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
+export interface TestCase {
+  input: string;
+  output: string;
+  isPublic: boolean; // true for visible to users, false for hidden test cases
+}
+
 @Schema({ timestamps: true })
 export class Problem extends Document<string> {
   @Prop({ required: true })
@@ -27,8 +33,8 @@ export class Problem extends Document<string> {
   @Prop()
   outputSpec: string;
 
-  @Prop({ type: Object })
-  samples: Record<string, string>[];
+  @Prop({ type: [Object], default: [] })
+  cases: TestCase[];
 
   @Prop({ default: [] })
   tags: string[];
