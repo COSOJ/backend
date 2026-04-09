67f6ac1e6e9e968258129f9d65a689a4e121e40b
Maifee Ul Asad <maifeeulasad@gmail.com>
2026-04-09 12:32:02 +0600

    [lint]: type express request user; reduce unsafe access;

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,7 +1,15 @@
 import { Request } from 'express';
 
+interface AuthenticatedUser {
+  userId?: string;
+  _id?: string;
+  roles?: string[];
+  handle?: string;
+  [key: string]: unknown;
+}
+
 declare module 'express' {
   interface Request {
-    user?: { userId: string };
+    user?: AuthenticatedUser;
   }
 }
