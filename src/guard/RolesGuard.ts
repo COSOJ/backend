2d9cdf2e5a9242563931671c9ecb6b643fc04b63
Maifee Ul Asad <maifeeulasad@gmail.com>
2026-04-09 12:35:27 +0600

    [lint]: type roles guard request access; remove unsafe role lookup;

--- src/guard/RolesGuard.ts
+++ src/guard/RolesGuard.ts
@@ -1,5 +1,6 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import { Request } from 'express';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
@@ -13,7 +14,7 @@ export class RolesGuard implements CanActivate {
     if (!requiredRoles) {
       return true;
     }
-    const { user } = context.switchToHttp().getRequest();
+    const { user } = context.switchToHttp().getRequest<Request>();
     return (
       user &&
       user.roles &&
