fc6505f3e088067fab3b31b18245b2d167d011c5
Maifee Ul Asad <maifeeulasad@gmail.com>
2026-04-09 13:01:41 +0600

    [lint]: switch auth service imports to relative paths for jest resolution;

--- src/service/auth.service.ts
+++ src/service/auth.service.ts
@@ -3,9 +3,9 @@ import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcryptjs';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { User } from 'src/schema/User';
-import { RegisterDto } from 'src/dto/auth/register.dto';
-import { LoginDto } from 'src/dto/auth/login.dto';
+import { User } from '../schema/User';
+import { RegisterDto } from '../dto/auth/register.dto';
+import { LoginDto } from '../dto/auth/login.dto';
 
 @Injectable()
 export class AuthService {
